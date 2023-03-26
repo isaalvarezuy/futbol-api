a4b60806c238fa300364a6a563ad9b62926c3852
isaalvarrez <87995315+isaalvarezuy@users.noreply.github.com>
2023-03-26 13:39:08 -0300

    linter and cors

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
