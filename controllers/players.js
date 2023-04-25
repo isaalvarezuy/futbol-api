863c03c179f2964ab8387051d4cbc4531bdd6c87
isaalvarrez <87995315+isaalvarezuy@users.noreply.github.com>
2023-04-25 17:02:14 -0300

    create controllers

---
+++ controllers/players.js
@@ -0,0 +1,51 @@
+const playersRouter = require('express').Router();
+const Player = require('../models/Player');
+const Team = require('../models/Team');
+
+const cloudinary = require('./cloudinary');
+
+playersRouter.get('/', async (req, res) => {
+    Player.find({}).populate('team').then(players => {
+        if (players) {
+            res.json(players);
+        } else {
+            return res.json({ message: 'Error' });
+        }
+    });
+});
+
+playersRouter.post('/', async (req, res) => {
+
+    const file = req.files.photo;
+
+    const image = await cloudinary.uploader.upload(file.tempFilePath, {
+        public_id: `${Date.now()}`,
+        resource_type: 'auto',
+        folder: 'football-app'
+    });
+
+    const { name, teamId, number } = req.body;
+
+
+    const player = new Player({
+        name,
+        imgUrl: image.url,
+        team: teamId,
+        goals: 0,
+        number,
+    });
+
+    const playerSaved = await player.save();
+
+    await Team.findByIdAndUpdate(
+        teamId,
+        { $push: { players: playerSaved._id } },
+        { new: true }
+    ).then(team => console.log(team));
+
+    res.json({ playerSaved });
+
+});
+
+
+module.exports = playersRouter;
---
+++ controllers/teams.js
@@ -0,0 +1,69 @@
+const teamsRouter = require('express').Router();
+const Team = require('../models/Team');
+
+const cloudinary = require('./cloudinary');
+
+teamsRouter.get('/', async (req, res) => {
+    Team.find({}).then(teams => {
+        if (teams) {
+            res.json(teams);
+        } else {
+            return res.json({ message: 'Error' });
+        }
+    });
+});
+
+teamsRouter.post('/', async (req, res) => {
+
+    const file = req.files.crest;
+
+    const image = await cloudinary.uploader.upload(file.tempFilePath, {
+        public_id: `${Date.now()}`,
+        resource_type: 'auto',
+        folder: 'football-app'
+    });
+
+    const team = new Team({
+        name: req.body.name,
+        imgUrl: image.url,
+        goalsFor: 0,
+        goalsAgainst: 0,
+        ties: 0,
+        loses: 0,
+        games: 0,
+        wins: 0,
+        gameHistory: [],
+    });
+
+    const productStored = await team.save();
+    res.json({ productStored });
+});
+
+teamsRouter.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    Team.findById(id).populate('players').then(team => {
+        if (team) {
+            return res.json(team);
+        } else {
+            return res.status(404).end();
+        }
+    }).catch((err) =>
+        next(err)
+    );
+});
+
+teamsRouter.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    Team.findByIdAndRemove(id).then(team => {
+        if (!team) {
+            res.status(404).end();
+        }
+        res.json(team);
+        res.status(204).end();
+    }
+    ).catch(err => next(err));
+
+});
+
+
+module.exports = teamsRouter;
